a8fc0a5e87285cf56f3fcfd01e1768f299f72e20
antoniofregoso <yomero@antoniofregoso.com>
2020-10-09 09:56:49 -0500

    [TMP] Clean modules

--- sale_real_estate/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).#
-from . import account_invoice
-from . import product
-from . import res_company
-from . import res_partner   
-from . import sale
-from . import stock
